04358a46bb0ddcd62f88cbfccce91c704129ccc8
Samuel E. Giddins <segiddins@segiddins.me>
2013-06-29 16:14:57 -0400

    Added XCProject

---
+++ SEGNetworkMock/SEGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SEGAppDelegate.h
+//  SEGNetworkMock
+//
+//  Created by Samuel E. Giddins on 6/29/13.
+//  Copyright (c) 2013 Samuel E. Giddins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
